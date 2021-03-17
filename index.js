eab7fc6c5b208309569d4fd55334a8c906a54956
Brandon Monge <oorekan@users.noreply.github.com>
2021-03-17 10:18:22 +0100

    Initialisation Node & Nom de l'équipe en couleur

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue("Brandon"));
+console.log(chalk.red("Jonathan"));
+console.log(chalk.yellow("Priscilia"));
+console.log(chalk.green("Ornella"));
